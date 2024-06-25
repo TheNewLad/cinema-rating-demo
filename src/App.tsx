0d0a06ba0f92f58d37e6e9825a769e8f5b5e33d8
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-06-24 20:50:50 -0500

    refactor: split layout for small and medium+ screens

--- src/App.tsx
+++ src/App.tsx
@@ -21,30 +21,69 @@ function App() {
   } = useRatings();
 
   return (
-    <main className="flex max-h-fit min-h-full flex-col gap-4 p-4 md:grid md:grid-cols-2 md:grid-rows-[min-content]">
-      <section className="flex max-h-fit justify-center md:col-start-1 md:col-end-2 md:row-start-1">
-        <h1 className="mb-5 text-5xl">Score</h1>
-      </section>
-      <section className="sticky top-0 z-50 flex justify-center bg-white md:relative md:col-start-1 md:col-end-2 md:row-start-2">
-        <Score
-          score={score}
-          useWeightedScore={useWeightedScore}
-          onChangeUseWeightedScore={setUseWeightedScore}
-        />
-      </section>
-      <section className="flex flex-col items-center gap-2.5 md:mb-0">
-        <CategoryWeightInputs
-          setCategoryWeight={setCategoryWeight}
-          weights={weights}
-        />
-      </section>
-      <section className="md:row-span-auto flex flex-col items-center gap-2.5 pb-2.5 md:col-start-2 md:col-end-3 md:row-span-2 md:row-start-1 md:h-full md:justify-end md:pb-0">
-        <CategoryRatingButtons
-          setCategoryRating={setCategoryRating}
-          ratings={ratings}
-        />
-      </section>
-    </main>
+    <>
+      {/* Mobile Layout */}
+      <main className="flex max-h-fit min-h-full flex-col gap-4 p-4 md:hidden md:grid-cols-2">
+        <section className="flex max-h-fit justify-center md:col-start-1 md:col-end-2 md:row-start-1">
+          <h1 className="mb-5 text-5xl">Score</h1>
+        </section>
+        <section className="sticky top-0 z-50 flex justify-center bg-white md:relative md:col-start-1 md:col-end-2 md:row-start-2">
+          <Score
+            score={score}
+            useWeightedScore={useWeightedScore}
+            onChangeUseWeightedScore={setUseWeightedScore}
+          />
+        </section>
+        <section
+          className={classNames(
+            "flex flex-col items-center gap-2.5 md:mb-0",
+            useWeightedScore || "hidden",
+          )}
+        >
+          <CategoryWeightInputs
+            setCategoryWeight={setCategoryWeight}
+            weights={weights}
+          />
+        </section>
+        <section className="md:row-span-auto flex flex-col items-center gap-2.5 pb-2.5 md:col-start-2 md:col-end-3 md:row-span-2 md:row-start-1 md:h-full md:justify-end md:pb-0">
+          <CategoryRatingButtons
+            setCategoryRating={setCategoryRating}
+            ratings={ratings}
+          />
+        </section>
+      </main>
+
+      {/* Desktop Layout */}
+      <main className="hidden max-h-fit min-h-full flex-col gap-4 p-4 md:grid md:grid-cols-2">
+        <section className="col-end-2 row-start-1 flex max-h-fit flex-col md:col-start-1">
+          <h1 className="mb-5 flex justify-center text-5xl">Score</h1>
+          <div className="flex justify-center bg-white">
+            <Score
+              score={score}
+              useWeightedScore={useWeightedScore}
+              onChangeUseWeightedScore={setUseWeightedScore}
+            />
+          </div>
+          <div
+            className={classNames(
+              "flex flex-col items-center gap-2.5 md:mb-0",
+              useWeightedScore || "hidden",
+            )}
+          >
+            <CategoryWeightInputs
+              setCategoryWeight={setCategoryWeight}
+              weights={weights}
+            />
+          </div>
+        </section>
+        <section className="flex flex-col items-center gap-2.5 pb-2.5">
+          <CategoryRatingButtons
+            setCategoryRating={setCategoryRating}
+            ratings={ratings}
+          />
+        </section>
+      </main>
+    </>
   );
 }
 
