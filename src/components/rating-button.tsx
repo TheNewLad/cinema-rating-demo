e684509a4ce7629e038ea1ca5432336f4fc0d20a
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-06-25 00:47:12 -0500

    style: improve rating button layout

--- src/components/rating-button.tsx
+++ src/components/rating-button.tsx
@@ -18,7 +18,7 @@ export const RatingButton = ({
     <button
       type="button"
       className={classNames(
-        "relative inline-flex flex-1 items-center bg-white px-3 py-2 text-sm font-normal text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-400 focus:z-10 dark:bg-slate-700 dark:text-white dark:ring-slate-500 dark:hover:bg-slate-600",
+        "relative inline-flex flex-1 items-center justify-evenly bg-white px-3 py-2 text-sm font-normal text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-400 focus:z-10 dark:bg-slate-700 dark:text-white dark:ring-slate-500 dark:hover:bg-slate-600",
         className,
         !selected ||
           "bg-slate-500 text-white hover:text-slate-950 dark:bg-slate-800 dark:hover:text-white",
