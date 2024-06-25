ff2fd5139c039cca2ce118d2c4ab479dd7b50c8c
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-06-24 20:52:00 -0500

    fix: make category weight input controlled

--- src/components/category-weight-inputs.tsx
+++ src/components/category-weight-inputs.tsx
@@ -25,7 +25,7 @@ export const CategoryWeightInputs = ({ weights, setCategoryWeight }: Props) => {
               className="block w-1/2 rounded-md border-0 p-1.5 text-right text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               name={id}
               type="number"
-              defaultValue={weights[id]}
+              value={weights[id]}
               onChange={(e) => setCategoryWeight(id, e.target.valueAsNumber)}
             />
           </span>
